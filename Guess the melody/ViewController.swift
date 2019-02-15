2a683ec4b96e841b27d79d82a86a0e1b2af00fae
Stanislav Mamulin <stanislavpm@mac-admin.local>
2019-02-15 23:47:02 +0700

    Initial Commit

---
+++ Guess the melody/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess the melody
+//
+//  Created by StanislavPM on 15/02/2019.
+//  Copyright © 2019 StanislavPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
