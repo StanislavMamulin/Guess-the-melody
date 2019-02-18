36ab9f4af86b9683c202bb44306bc55a522f8180
Stanislav Mamulin <stanislavpm@mac-admin.local>
2019-02-18 11:51:38 +0700

    Delete some comments

--- Guess the melody/URL+Extension.swift
+++ Guess the melody/URL+Extension.swift
@@ -10,8 +10,7 @@ import Foundation
 
 extension URL {
     func withQueries(_ queries: [String : String]) -> URL? {
-        // URL - хорош тем, что в струткуре отделены различные части адреса. Host - apple.com, различные компоненты пути и т.д.
-        var components = URLComponents(url: self, resolvingAgainstBaseURL: true) // resolvingAgainstBaseURL - только дополнение или нет, true - полный адреса
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
         
         components?.queryItems = queries.map {
             URLQueryItem(name: $0.key, value: $0.value)
